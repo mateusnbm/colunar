6465d92652a8b547eeb832400ddda3b5459a945a
mateusnbm <mnbm@cin.ufpe.br>
2020-01-12 01:39:31 -0300

    Test query using JOIN, GROUP BY, ORDER BY and WHERE with spatial-based condition.

--- apps/generate-geoms/test/geospark.scala
+++ apps/generate-geoms/test/geospark.scala
@@ -7,47 +7,156 @@ import org.apache.hadoop.hbase.spark.datasources.HBaseTableCatalog
 import org.datasyslab.geosparksql.utils.GeoSparkSQLRegistrator
 import org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator
 
-val spark = SparkSession.
+val session = 
+    SparkSession.
     builder().
-    appName("HBaseSourceExample").
+    appName("GeosparkSample").
     config("spark.serializer", classOf[KryoSerializer].getName).
     config("spark.kryo.registrator", classOf[GeoSparkVizKryoRegistrator].getName).
     getOrCreate()
 
-GeoSparkSQLRegistrator.registerAll(spark)
+GeoSparkSQLRegistrator.registerAll(session)
 
-def catalog =
-  s"""{
-      |"table":{"namespace":"default", "name":"people"},
-      |"rowkey":"key",
-      |"columns":{
-      |"PID":{"cf":"rowkey", "col":"key", "type":"string"},
-      |"NAME":{"cf":"cf", "col":"NAME", "type":"string"},
-      |"SURNAME":{"cf":"cf", "col":"SURNAME", "type":"string"},
-      |"BIRTHDAY":{"cf":"cf", "col":"BIRTHDAY", "type":"string"},
-      |"BIRTH_LAT":{"cf":"cf", "col":"BIRTH_LAT", "type":"string"},
-      |"BIRTH_LON":{"cf":"cf", "col":"BIRTH_LON", "type":"string"},
-      |"BIRTH_POINT":{"cf":"cf", "col":"BIRTH_POINT", "type":"string"}
-      |}
-      |}""".stripMargin
+def peopleCatalog =
+    s"""{
+        |"table":{
+            |"namespace":"default", 
+            |"name":"people"
+        |},
+        |"rowkey":"key",
+        |"columns":{
+            |"PID":{"cf":"rowkey", "col":"key", "type":"string"},
+            |"NAME":{"cf":"cf", "col":"NAME", "type":"string"},
+            |"SURNAME":{"cf":"cf", "col":"SURNAME", "type":"string"},
+            |"BIRTHDAY":{"cf":"cf", "col":"BIRTHDAY", "type":"string"},
+            |"BIRTH_LAT":{"cf":"cf", "col":"BIRTH_LAT", "type":"string"},
+            |"BIRTH_LON":{"cf":"cf", "col":"BIRTH_LON", "type":"string"},
+            |"BIRTH_POINT":{"cf":"cf", "col":"BIRTH_POINT", "type":"string"}
+        |}
+    |}""".stripMargin
 
-val df = spark.read.option(HBaseTableCatalog.tableCatalog, catalog).format("org.apache.spark.sql.execution.datasources.hbase").load()
-df.createOrReplaceTempView("df")
-df.show()
+def countriesCatalog =
+    s"""{
+        |"table":{
+            |"namespace":"default", 
+            |"name":"countries"
+        |},
+        |"rowkey":"key",
+        |"columns":{
+            |"ID":{"cf":"rowkey", "col":"key", "type":"string"},
+            |"ISO":{"cf":"cf", "col":"ISO", "type":"string"},
+            |"NAME":{"cf":"cf", "col":"NAME", "type":"string"},
+            |"GEOM":{"cf":"cf", "col":"GEOM", "type":"string"}
+        |}
+    |}""".stripMargin
 
-var cq = 
+def statesCatalog =
+    s"""{
+        |"table":{
+            |"namespace":"default", 
+            |"name":"states"
+        |},
+        |"rowkey":"key",
+        |"columns":{
+            |"ID":{"cf":"rowkey", "col":"key", "type":"string"},
+            |"NAME":{"cf":"cf", "col":"NAME", "type":"string"},
+            |"COUNTRY_ISO":{"cf":"cf", "col":"COUNTRY_ISO", "type":"string"},
+            |"COUNTRY_NAME":{"cf":"cf", "col":"COUNTRY_NAME", "type":"string"},
+            |"GEOM":{"cf":"cf", "col":"GEOM", "type":"string"}
+        |}
+    |}""".stripMargin
+
+val peopleDataframe = 
+    session.
+    read.option(HBaseTableCatalog.tableCatalog, peopleCatalog).
+    format("org.apache.spark.sql.execution.datasources.hbase").
+    load()
+
+val countriesDataframe = 
+    session.
+    read.option(HBaseTableCatalog.tableCatalog, countriesCatalog).
+    format("org.apache.spark.sql.execution.datasources.hbase").
+    load()
+
+val statesDataframe = 
+    session.
+    read.option(HBaseTableCatalog.tableCatalog, statesCatalog).
+    format("org.apache.spark.sql.execution.datasources.hbase").
+    load()
+
+peopleDataframe.createOrReplaceTempView("peopleDataframe")
+countriesDataframe.createOrReplaceTempView("countriesDataframe")
+statesDataframe.createOrReplaceTempView("statesDataframe")
+
+peopleDataframe.show()
+countriesDataframe.show()
+statesDataframe.show()
+
+var peopleMakeGeomQuery = 
     s"""
-        |SELECT PID, NAME, SURNAME, BIRTHDAY, BIRTH_LAT, BIRTH_LON, ST_GeomFromWKT(BIRTH_POINT) AS BIRTH_POINT
-        |FROM df
+    |SELECT 
+        |PID, 
+        |NAME, 
+        |SURNAME, 
+        |BIRTHDAY, 
+        |BIRTH_LAT, 
+        |BIRTH_LON, 
+        |ST_GeomFromWKT(BIRTH_POINT) AS BIRTH_POINT
+    |FROM peopleDataframe
     """.stripMargin
 
-var spatialDf = spark.sql(cq)
-spatialDf.createOrReplaceTempView("spatialdf")
-spatialDf.printSchema()
-spatialDf.show()
+var countriesMakeGeomQuery = 
+    s"""
+    |SELECT 
+        |ID, 
+        |ISO, 
+        |NAME, 
+        |ST_GeomFromWKT(GEOM) AS GEOM
+    |FROM countriesDataframe
+    """.stripMargin
 
-var query_1 = "SELECT NAME, BIRTH_POINT, ST_Distance (BIRTH_POINT, ST_GeomFromWKT('POINT(-45.7780 -12.2368)')  ) AS DIST FROM spatialdf"
-var query_1_df = spark.sql(query_1)
-query_1_df.createOrReplaceTempView("query_1_df")
-query_1_df.show()
+var statesMakeGeomQuery = 
+    s"""
+    |SELECT 
+        |ID, 
+        |NAME, 
+        |COUNTRY_ISO, 
+        |COUNTRY_NAME, 
+        |ST_GeomFromWKT(GEOM) AS GEOM
+    |FROM statesDataframe
+    """.stripMargin
 
+var spatialPeopleDataframe = session.sql(peopleMakeGeomQuery)
+var spatialCountriesDataframe = session.sql(countriesMakeGeomQuery)
+var spatialStatesDataframe = session.sql(statesMakeGeomQuery)
+
+spatialPeopleDataframe.createOrReplaceTempView("spatialPeopleDataframe")
+spatialPeopleDataframe.printSchema()
+spatialPeopleDataframe.show()
+
+spatialCountriesDataframe.createOrReplaceTempView("spatialCountriesDataframe")
+spatialCountriesDataframe.printSchema()
+spatialCountriesDataframe.show()
+
+spatialStatesDataframe.createOrReplaceTempView("spatialStatesDataframe")
+spatialStatesDataframe.printSchema()
+spatialStatesDataframe.show()
+
+var birthsPerStateQuery = 
+    s"""
+    |SELECT 
+        |S.COUNTRY_NAME,
+        |S.NAME, 
+        |COUNT(*) AS BIRTHS
+    |FROM
+        |spatialPeopleDataframe P,
+        |spatialStatesDataframe S
+    |WHERE
+        |ST_Contains(S.GEOM, P.BIRTH_POINT)
+    |GROUP BY
+        |S.COUNTRY_NAME,
+        |S.NAME
+    |ORDER BY S.NAME ASC
+    """.stripMargin
+
+session.sql(birthsPerStateQuery).show()
